4e6d2ccc0e511bc7ceb79875a486ebe24c5397c7
gyojunChae <122966780+gyojunChae@users.noreply.github.com>
2023-12-26 19:53:01 +0900

    2차 수정
    2차수정

--- js/index.js
+++ js/index.js
@@ -1,16 +1,27 @@
 const 정답 = "APPLE";
 let attempts = 0;
 let index = 0;
+let timer;
 
 function appStart() {
-  const nextLine = () => {
-    if (attempts === 6) return;
-    attempts += 1;
-    index = 0;
+  const displayGameover = () => {
+    const div = document.createElement("div");
+    div.innerText = "게임이 종료됬습니다.";
+    div.style =
+      "display:flex; justify-content:center; align-items:center; position:absolute; top:40vh; left:38%; background-color:white; width:200px; height:100px;";
+    document.body.appendChild(div);
   };
 
   const gameover = () => {
     window.removeEventListener("keydown", handleKeydown);
+    displayGameover();
+    clearInterval(timer);
+  };
+
+  const nextLine = () => {
+    if (attempts === 6) return;
+    attempts += 1;
+    index = 0;
   };
 
   const handleEnterKey = () => {
@@ -33,6 +44,16 @@ function appStart() {
     else nextLine();
   };
 
+  const handleBackspace = () => {
+    if (index > 0) {
+      const preBlock = document.querySelector(
+        `.board-column[data-index='${attempts}${index - 1}']`
+      );
+      preBlock.innerText = "";
+    }
+    if (index != 0) index -= 1;
+  };
+
   const handleKeydown = (event) => {
     console.log("키가 입력!", event.key);
     const key = event.key.toUpperCase();
@@ -41,7 +62,8 @@ function appStart() {
       `.board-column[data-index='${attempts}${index}']`
     );
 
-    if (index === 5) {
+    if (event.key === "Backspace") handleBackspace();
+    else if (index === 5) {
       if (event.key === "Enter") handleEnterKey();
       else return;
     } else if (65 <= keyCode && keyCode <= 90) {
@@ -50,6 +72,21 @@ function appStart() {
     }
   };
 
+  const startTimer = () => {
+    const 시작_시간 = new Date();
+    function setTime() {
+      const 현재_시간 = new Date();
+      const 흐른_시간 = new Date(현재_시간 - 시작_시간);
+      const 분 = 흐른_시간.getMinutes().toString().padStart(2, "0");
+      const 초 = 흐른_시간.getSeconds().toString().padStart(2, "0");
+      const timediv = document.querySelector("#timer");
+      timediv.innerText = `${분}:${초}`;
+    }
+
+    timer = setInterval(setTime, 1000);
+  };
+
+  startTimer();
   window.addEventListener("keydown", handleKeydown);
 }
 
